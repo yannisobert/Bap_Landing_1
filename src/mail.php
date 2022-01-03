917a753eee2e8d2318855aba08826bf7e88b5de3
yannisobert <90550556+yannisobert@users.noreply.github.com>
2022-01-03 09:10:29 +0100

    :tada: Add mail php

---
+++ src/mail.php
@@ -0,0 +1,16 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "obertyannis@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+    "CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
