b907216e62f442c5cd71c51eb5ede9eac7b13294
yannisobert <90550556+yannisobert@users.noreply.github.com>
2022-01-06 11:19:19 +0100

    :hammer: Update php maim

--- src/mail.php
+++ src/mail.php
@@ -4,11 +4,12 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 $name = $_POST['name'];
+$surname = $_POST['surname'];
 $email= $_POST['email'];
 $message= $_POST['message'];
 $to = "obertyannis@gmail.com";
 $subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$txt ="Name = ". $name . "\r\n Surname = " . $surname . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
     "CC: somebodyelse@example.com";
 if($email!=NULL){
